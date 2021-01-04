2e1aebf5d27b8da46057936ca4c2a9deb2a91066
leonvanderhaeghen <leon.vanderhaeghen@hotmail.com>
2021-01-04 21:57:05 +0000

    added most of the genises file

--- x/mineservice/genesis.go
+++ x/mineservice/genesis.go
@@ -9,8 +9,17 @@ import (
 
 // InitGenesis initialize default parameters
 // and the keeper's address to pubkey map
-func InitGenesis(ctx sdk.Context, k keeper.Keeper /* TODO: Define what keepers the module needs */, data types.GenesisState) {
+func InitGenesis(ctx sdk.Context, k keeper.Keeper, data types.GenesisState) {
 	// TODO: Define logic for when you would like to initalize a new genesis
+	for _, record := range data.MineRecords {
+		k.SetMine(ctx, record)
+	}
+	for _, record := range data.ResourceRecords {
+		k.SetResource(ctx, record)
+	}
+		for _, record := range data.PlayerRecords {
+		k.SetPlayer(ctx, record)
+	}
 }
 
 // ExportGenesis writes the current store values
--- x/mineservice/types/genesis.go
+++ x/mineservice/types/genesis.go
@@ -1,14 +1,20 @@
 package types
 
+import "fmt"
+
 // GenesisState - all mineservice state that must be provided at genesis
 type GenesisState struct {
-	// TODO: Fill out what is needed by the module for genesis
+	MineRecords []Mine `json:"mine_records"`
+	PlayerRecords []Player `json:"player_records"`
+	ResourceRecords []Resource `json:"resource_records"`
 }
 
 // NewGenesisState creates a new GenesisState object
 func NewGenesisState( /* TODO: Fill out with what is needed for genesis state */ ) GenesisState {
 	return GenesisState{
-		// TODO: Fill out according to your genesis state
+		PlayerRecords: nil,
+		MineRecords: nil,
+		ResourceRecords: nil,
 	}
 }
 
@@ -16,11 +22,33 @@ func NewGenesisState( /* TODO: Fill out with what is needed for genesis state */
 func DefaultGenesisState() GenesisState {
 	return GenesisState{
 		// TODO: Fill out according to your genesis state, these values will be initialized but empty
+		PlayerRecords: []Player{},
+		MineRecords: []Mine{},
+		ResourceRecords: []Resource{},
+
 	}
 }
 
 // ValidateGenesis validates the mineservice genesis parameters
 func ValidateGenesis(data GenesisState) error {
 	// TODO: Create a sanity check to make sure the state conforms to the modules needs
+	for _, record := range data.PlayerRecords {
+		if record.Creator == nil {
+			return fmt.Errorf("invalid PlayerRecord: Creator: %s. Error: Missing Creator", record.Creator)
+		}
+	}
+	for _, record := range data.MineRecords {
+		if record.Owner == nil {
+			return fmt.Errorf("invalid MineRecord: Owner: %s. Error: Missing Owner", record.Owner)
+		}
+	}
+	for _, record := range data.ResourceRecords {
+		if record.Owner == nil {
+			return fmt.Errorf("invalid ResourceRecord: Owner: %s. Error: Missing Owner", record.Owner)
+		}
+		if record.Name == "" {
+			return fmt.Errorf("invalid ResourceRecord: Owner: %s. Error: Missing Name", record.Name)
+		}
+	}
 	return nil
 }
